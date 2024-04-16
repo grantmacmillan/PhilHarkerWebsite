d6ecd09c73201ed673857b42055a09c2e2006603
Philip <philharker88@gmail.com>
2024-04-16 16:55:53 -0400

    css changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,12 @@
 import PhilipsSelfie from "../assets/philip-selfie.png"
+import styles from "../styles/ScreenStyles.module.css"
 
 const HomeScreen = ({}) => {
 
     return(
         <div>
             <h1>Home Page</h1>
-            <img src={PhilipsSelfie}></img>
+            <img src={PhilipsSelfie} width="450"></img>
             <br></br>
 
             <div>Hello. My name is Philip. I do some stuff.</div>
@@ -13,6 +14,7 @@ const HomeScreen = ({}) => {
             <br></br>
 
             <a href="https://philidips.substack.com">My Newsletter</a> — <a href="/contact">My Contact</a>
+
         </div>
     );
 };
