80d366c4bd5cb98a1c098b3522a686f506cf8925
Philip <philharker88@gmail.com>
2024-05-09 19:41:37 -0400

    colour experimentation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,23 +6,25 @@ const HomeScreen = () => {
     return (
         <div className="container my-4">
             
-            <Container style={{textAlign: 'center'}} className="hero">
+            <Container className="hero">
             <h1 className="mb-3">Home Page</h1>
             <img src={PhilipsSelfie} width="450" className="img-fluid mb-3"></img>
 
             <p className="lead">Hello. My name is Philip. I do a bit of everything.</p>
 
             <div>
-                <a href="https://philidips.substack.com" className="btn btn-primary me-2">My Newsletter</a>
-                <a href="/contact" className="btn btn-secondary">My Contact</a>
+                <a href="https://philidips.substack.com" className="btn button-default">My Newsletter</a>
+                <a href="/contact" className="btn button-default">My Contact</a>
             </div>
             </Container>
 
-            <Container>
-            <h2 style={{textAlign: 'center'}}>How can I help you today?</h2>
+            <hr/>
+
+            <Container className="directory">
+            <h1 style={{textAlign: 'center', marginBottom: '2%'}}>How can I help you today?</h1>
                 <Row>
                     <Col>
-                        <Card>
+                        <Card className="directory-card">
                             <h3>I want to commission writing from Philip.</h3>
                             <CardBody>
                                 Well, thank you! The Writing page contains samples of my past work across all genres and media.
@@ -30,7 +32,7 @@ const HomeScreen = () => {
                         </Card>
                     </Col>
                     <Col>
-                        <Card>
+                        <Card className="directory-card">
                             <h3>I want to see Philip's games and programming.</h3>
                             <CardBody>
                                 The Software page covers all of that, both personal and professional.
@@ -38,7 +40,7 @@ const HomeScreen = () => {
                         </Card>
                     </Col>
                     <Col>
-                        <Card>
+                        <Card className="directory-card">
                             <h3>I want to see Philip's scientific CV.</h3>
                             <CardBody>
                                 From published work to particularly good school projects, it's all in the Ecology page.
